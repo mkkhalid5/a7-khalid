e342388aef8e1761eb0d7050fbf813c899feca40
mkkhalid5 <141407565+mkkhalid5@users.noreply.github.com>
2026-04-14 18:05:54 +0600

    add empty interaction in stats page

--- src/pages/stats/Stats.jsx
+++ src/pages/stats/Stats.jsx
@@ -5,7 +5,7 @@ import { FriendContext } from '../../context/FriendContext';
 
 
 const Stats = () => {
-    const {friendContact} = useContext(FriendContext);
+    const { friendContact } = useContext(FriendContext);
 
     const callCount = friendContact.filter(item => item.actionType === "call").length;
     const textCount = friendContact.filter(item => item.actionType === "text").length;
@@ -18,30 +18,34 @@ const Stats = () => {
     ]
 
     return (
+
         <div className='py-20 container mx-auto px-3 space-y-6'>
             <h2 className='text-3xl md:text-5xl font-bold'>Friendship Analytics</h2>
-
             <div className='p-8 space-y-6 bg-white rounded-md shadow'>
                 <h2 className='text-[#244D3F]'>By Interaction Type</h2>
+                 
+                {
+                    friendContact.length === 0 ? <div className='text-center'>No interactions created.!</div> :
+                    <div className='flex justify-center'>
+                        <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '40vh', aspectRatio: 1 }} responsive>
+                            <Pie
+                                data={data}
+                                innerRadius="80%"
+                                outerRadius="100%"
+                                // Corner radius is the rounded edge of each pie slice
+                                cornerRadius="50%"
+                                fill="#8884d8"
+                                // padding angle is the gap between each pie slice
+                                paddingAngle={5}
+                                dataKey="value"
+                                isAnimationActive={true}
+                            />
+                            <Legend iconType='circle' />
+                            <Tooltip />
+                        </PieChart>
+                    </div>
+                }
 
-                <div className='flex justify-center'>
-                    <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '40vh', aspectRatio: 1 }} responsive>
-                        <Pie
-                            data={data}
-                            innerRadius="80%"
-                            outerRadius="100%"
-                            // Corner radius is the rounded edge of each pie slice
-                            cornerRadius="50%"
-                            fill="#8884d8"
-                            // padding angle is the gap between each pie slice
-                            paddingAngle={5}
-                            dataKey="value"
-                            isAnimationActive={true}
-                        />
-                        <Legend iconType='circle' />
-                        <Tooltip />
-                    </PieChart>
-                </div>
             </div>
         </div>
     );
